856e9e3f0f3968a73be07ef11df80f813a161e68
steveYeah <steve.hutchins@gousto.co.uk>
2022-02-25 14:50:06 +0000

    Add boolean, less than and variable expressions and add an environment to the machine to use when evaluating variables

--- ch2_meaning_of_programs/operational_semantics/small_step_semantics/SIMPLE/expressions.rb
+++ ch2_meaning_of_programs/operational_semantics/small_step_semantics/SIMPLE/expressions.rb
@@ -12,16 +12,48 @@ class Number < Struct.new(:value)
   end
 end
 
+class Boolean < Struct.new(:value)
+  def reducible?
+    false
+  end
+
+  def to_s
+    value.to_s
+  end
+
+  def inspect
+    "<<#{self}>>"
+  end
+end
+
+class Variable < Struct.new(:name)
+  def reducible?
+    true
+  end
+
+  def reduce(environment)
+    environment[name]
+  end
+
+  def to_s
+    name.to_s
+  end
+
+  def inspect
+    "<<#{self}>>"
+  end
+end
+
 class Add < Struct.new(:left, :right)
   def reducible?
     true
   end
 
-  def reduce
+  def reduce(environment)
     if left.reducible?
-      Add.new(left.reduce, right)
+      Add.new(left.reduce(environment), right)
     elsif right.reducible?
-      Add.new(left, right.reduce)
+      Add.new(left, right.reduce(environment))
     else
       Number.new(left.value + right.value)
     end
@@ -34,7 +66,6 @@ class Add < Struct.new(:left, :right)
   def inspect
     "<<#{self}>>"
   end
-
 end
 
 class Multiply < Struct.new(:left, :right)
@@ -42,11 +73,11 @@ class Multiply < Struct.new(:left, :right)
     true
   end
 
-  def reduce
+  def reduce(environment)
     if left.reducible?
-      Multiply.new(left.reduce, right)
+      Multiply.new(left.reduce(environment), right)
     elsif right.reducible?
-      Multiply.new(left, right.reduce)
+      Multiply.new(left, right.reduce(environment))
     else
       Number.new(left.value * right.value)
     end
@@ -54,6 +85,30 @@ class Multiply < Struct.new(:left, :right)
 
   def to_s
     "#{left} * #{right}"
+  end
+
+  def inspect
+    "<<#{self}>>"
+  end
+end
+
+class LessThan < Struct.new(:left, :right)
+  def reducible?
+    true
+  end
+
+  def reduce(environment)
+    if left.reducible?
+      LessThan.new(left.reduce(environment), right)
+    elsif right.reducible?
+      LessThan.new(left, right.reduce(environment))
+    else
+      Boolean.new(left.value < right.value)
+    end
+  end
+
+  def to_s
+    "#{left} < #{right}"
   end
 
   def inspect
